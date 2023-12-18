5545f6d85e8915ad2943d995894c28b48578e1d3
Ash <khayrul.ashsmith@outlook.com>
2023-12-17 21:13:43 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,23 @@ module.exports = {
   description: "Ping pong command",
   type: CommandType.SLASH,
 
-  callback: ({ client, guild, interaction }) => {
-    interaction.reply({ embeds: [{ description: `I'm still Alive`}], fetchReply: true });
+  callback: async ({ client, guild, interaction }) => {
+    const startTime = Date.now();
+    const reply = await interaction.reply({
+      embeds: [{ description: "Pinging..." }],
+      fetchReply: true,
+    });
+    const endTime = Date.now();
+    const ping = endTime - startTime;
+
+    reply.edit({
+      embeds: [
+        {
+          description: `Pong! Latency is ${ping}ms, API Latency is ${Math.round(
+            client.ws.ping
+          )}ms`,
+        },
+      ],
+    });
   },
-}
+};
