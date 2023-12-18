1910530f55d588eb96dcbf1838635bff10c84984
Anush <33168386+AnushK-Fro@users.noreply.github.com>
2023-12-18 00:36:15 -0500

    Update log.js

--- events/guildCreate/log.js
+++ events/guildCreate/log.js
@@ -1,11 +1,1 @@
-// Description: When the bot joins a guild, log it to the console. and add the guild to the database.
-const { Settings } = require("../../index.js");
-module.exports = (guild) => {
-    console.log(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot.`);
-    Settings.create({
-        guild: guild.id,
-        botname: "AdvinServers Stock",
-        stockmontiorchannel: "none",
-        updateinterval: 0,
-    });
-};
+console.log('HOLD UP');
